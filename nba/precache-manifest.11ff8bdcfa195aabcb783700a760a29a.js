e3e9f969aed475c72fa95ba4be0032bafadb4e3f
Travis CI User <travis@example.org>
2018-10-22 03:29:27 +0000

    update nba-daily

---
+++ nba/precache-manifest.11ff8bdcfa195aabcb783700a760a29a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c921aa51f0c490814754",
+    "url": "/nba/static/js/runtime~main.c921aa51.js"
+  },
+  {
+    "revision": "d7de3e5fd369ed3edff8",
+    "url": "/nba/static/js/main.d7de3e5f.chunk.js"
+  },
+  {
+    "revision": "0b66dbaaf078ff560937",
+    "url": "/nba/static/js/1.0b66dbaa.chunk.js"
+  },
+  {
+    "revision": "d7de3e5fd369ed3edff8",
+    "url": "/nba/static/css/main.4633fd21.chunk.css"
+  },
+  {
+    "revision": "3274cd33c6e0a54f52a8bc6feefebacf",
+    "url": "/nba/index.html"
+  }
+];
