c78521122bb5b341157d8809ada46d5df6206c99
Travis CI User <travis@example.org>
2019-01-10 03:45:14 +0000

    update nba-daily

---
+++ nba/precache-manifest.cf2e6aceeea5505dd3664d37af87f4c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c921aa51f0c490814754",
+    "url": "/nba/static/js/runtime~main.c921aa51.js"
+  },
+  {
+    "revision": "9089df83d282e9329bec",
+    "url": "/nba/static/js/main.9089df83.chunk.js"
+  },
+  {
+    "revision": "438d448c9a4ba9cbf812",
+    "url": "/nba/static/js/1.438d448c.chunk.js"
+  },
+  {
+    "revision": "9089df83d282e9329bec",
+    "url": "/nba/static/css/main.0cac96d9.chunk.css"
+  },
+  {
+    "revision": "08bc7d07ccb2459884a36d95b3d55032",
+    "url": "/nba/index.html"
+  }
+];
--- nba/service-worker.js
+++ nba/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/nba/precache-manifest.2df6799cb1e9d873823bdc8950353218.js"
+  "/nba/precache-manifest.cf2e6aceeea5505dd3664d37af87f4c7.js"
 );
 
 workbox.clientsClaim();
