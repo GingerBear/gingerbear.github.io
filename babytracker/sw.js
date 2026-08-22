0b32be19cb70072d51be9b787bae37a09189fcc5
GingerBear <bbbear444@gmail.com>
2026-08-22 00:12:20 -0400

    Deploy BabyTracker PWA (8/22/2026, 12:12:20 AM)

--- babytracker/sw.js
+++ babytracker/sw.js
@@ -5,7 +5,7 @@
  * Caches core application assets for instant 0ms offline launch.
  */
 
-const CACHE_NAME = 'babytracker-v1787371313383';
+const CACHE_NAME = 'babytracker-v1787371940306';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
